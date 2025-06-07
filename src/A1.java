02e128ba56c8952afb85e1a31055b1b6745f3722
ommistry223 <ommistry5559@gmail.com>
2025-06-07 23:23:58 +0530

    Initial commit

---
+++ src/A1.java
@@ -0,0 +1,10 @@
+public class A1 {
+    public static void main(String[] args) {
+        a(args);  // Correct method name
+    }
+
+    public static void a(String[] run) {
+        char i = 'A';
+        System.out.println("The character is: " + i);
+    }
+}
---
+++ src/First.java
@@ -0,0 +1,10 @@
+public class First {
+    public static void main(String[] args) {
+        first(args);  // calling the static method
+    }
+
+    public static void first(String[] run) {
+        char i = 'A';
+        System.out.println("The character is: " + i);
+    }
+}
