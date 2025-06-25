8bd4f1e76ad4e4ed072d04ea115ded9182a3a337
ommistry223 <ommistry5559@gmail.com>
2025-06-25 21:21:56 +0530

    Initial commit

---
+++ src/Loop.java
@@ -0,0 +1,13 @@
+public class Loop {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("4 th Table");
+
+       for(int i=1;i<=10;i++) {
+           System.out.println("4" + " * " + i + "= " + 4 * i);
+       }
+
+    }
+}
